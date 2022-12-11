afbd184730bd0bedb147f2ede8b94807dfe91c4a
shathadbe <shathaaldbee@gmail.com>
2022-12-11 10:45:08 +0200

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+//import { environment } from './../enviroments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
